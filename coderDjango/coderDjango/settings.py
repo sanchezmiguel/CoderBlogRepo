017a8f0bd626f802f77d483c6f4b0b74accb1f6d
Miguel Sanchez <msanchez@impulsedynamics.com>
2023-10-26 19:59:31 -0300

    creo Apps blog y perfiles
    App blog: para gestionar lo relativo a los artículos
    App perfiles: para gestionar registro de usuarios y login

--- coderDjango/coderDjango/settings.py
+++ coderDjango/coderDjango/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Mis Apps
+    'blogApp',
+    'perfilesApp',
 ]
 
 MIDDLEWARE = [
---
+++ coderDjango/perfilesApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PerfilesappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'perfilesApp'
