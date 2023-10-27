151c9f401bb3bff060fbe8ba999e42bf6fb29bf4
Miguel Sanchez <msanchez@impulsedynamics.com>
2023-10-27 12:45:36 -0300

    Home Ruta base “/” Visualizar el home del blog.

--- coderDjango/coderDjango/urls.py
+++ coderDjango/coderDjango/urls.py
@@ -18,9 +18,11 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('blogApp.urls')),
     path('pages/',include('blogApp.urls')),
     path('accounts/',include('perfilesApp.urls')),
 ]
