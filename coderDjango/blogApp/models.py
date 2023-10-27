686019a083f869d1843d6beb4e33c6256739b6ac
Miguel Sanchez <msanchez@impulsedynamics.com>
2023-10-27 10:00:46 -0300

    Se agrega Bootstrap se corrige modelo para que Text sea TextField se genera script para generar articulos de prueba

--- coderDjango/blogApp/models.py
+++ coderDjango/blogApp/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class Articulo(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.CharField(max_length=200)
-    text = models.CharField(max_length=200)
+    text = models.TextField(blank=True)
     image = models.ImageField(upload_to='articulo_images/', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articulos")
