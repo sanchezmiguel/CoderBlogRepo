32044e78bc516f9a0e4b5e67618ce1c1252cc94d
Miguel Sanchez <msanchez@impulsedynamics.com>
2023-11-02 09:18:31 -0300

    se corrige barra superior de listado de articulos

--- coderDjango/blogApp/models.py
+++ coderDjango/blogApp/models.py
@@ -1,16 +1,20 @@
-# from ckeditor.fields import RichTextField
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
-# Create your models here.
 class Articulo(models.Model):
-    title = models.CharField(max_length=200)
-    subtitle = models.CharField(max_length=200)
-    text = models.TextField(blank=True)
-    image = models.ImageField(upload_to='articulo_images/', blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articulos")
+    title = models.CharField(max_length=200, help_text="Título del artículo.")
+    subtitle = models.CharField(max_length=200, help_text="Subtítulo del artículo.")
+    text = models.TextField(blank=True, help_text="Texto del artículo. Puede estar en blanco si es necesario.")
+    image = models.ImageField(upload_to='articulo_images/', blank=True, null=True,
+                              help_text="Imagen asociada al artículo.")
+    created = models.DateTimeField(auto_now_add=True, help_text="Fecha y hora de creación del artículo.")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articulos",
+                               help_text="Autor del artículo. Enlazado a un usuario.")
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = "Artículo"
+        verbose_name_plural = "Artículos"
--- coderDjango/mensajeriaApp/models.py
+++ coderDjango/mensajeriaApp/models.py
@@ -3,17 +3,19 @@ from django.contrib.auth.models import User
 
 
 class Chat(models.Model):
-    usuario_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats1')
-    usuario_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats2')
+    usuario_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats1',
+                                  help_text="Usuario 1 en el chat.")
+    usuario_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats2',
+                                  help_text="Usuario 2 en el chat.")
 
     def __str__(self):
         return f"Chat entre {self.usuario_1} y {self.usuario_2}"
 
 
 class Message(models.Model):
-    fecha_creacion = models.DateTimeField(auto_now_add=True)
-    texto = models.TextField()
-    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    fecha_creacion = models.DateTimeField(auto_now_add=True, help_text="Fecha y hora de creación del mensaje.")
+    texto = models.TextField(help_text="Texto del mensaje.")
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, help_text="Chat al que pertenece este mensaje.")
 
     def __str__(self):
         return f"{self.fecha_creacion.strftime('%Y-%m-%d %H:%M')} - {self.texto}"
